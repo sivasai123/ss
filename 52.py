10ef39cadd281d0022409f73b11dc5d0b9294680
sivasai123 <52394434+sivasai123@users.noreply.github.com>
2019-07-29 04:10:15 -0700

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,23 @@
+n=int(input())
+if n==1:
+	print('One')
+elif n==2:
+	print('Two')
+elif n==3:
+	print('Three')
+elif n==4:
+	print('Four')
+elif n==5:
+	print('Five')
+elif n==6:
+	print('Six')
+elif n==7:
+	print('Seven')
+elif n==8:
+	print('Eigth')
+elif n==9:
+	print('Nine')
+elif n==10:
+	print('Ten')
+else:
+	print('Invalid')
